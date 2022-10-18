e3fc8936f440f51b85cac22f011145fe84dd84b9
KorvinDarkus <pavel.bityutskiy@mail.ru>
2022-10-19 00:54:04 +0300

    В первом задании допилил расчёт(работает)

--- Lab2/mainwindow.cpp
+++ Lab2/mainwindow.cpp
@@ -13,3 +13,10 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_ex1Button_clicked()
+{
+    double p[6] = {1 - ui->inpt1->value(), 1 - ui->inpt2->value(), 1 - ui->inpt3->value(), 1 - ui->inpt4->value(), 1 - ui->inpt5->value(), 1 - ui->inpt6->value()};
+    ui->outpt->setNum( 1 - (p[0]*(p[1]*(p[2] + p[3] - p[2]*p[3]) + p[4]*p[5] - p[1]*p[4]*p[5]*(p[2] + p[3] - p[2]*p[3])))) ;
+
+};
